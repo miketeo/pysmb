016407acebfe4f6d0722dd332773fcc31bc64784
Michael Teo <miketeo@miketeo.net>
2013-05-31 06:26:07 +0000

    Update revision and change log

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ pkgdir = { '': 'python%s' % sys.version_info[0] }
 
 setup(
     name = "pysmb",
-    version = "1.1.3",
+    version = "1.1.4",
     author = "Michael Teo",
     author_email = "miketeo@miketeo.net",
     license = "zlib/libpng",
