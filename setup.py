831a52c9ead6ab3d2ba7eca7ee4bd1abfb34381f
Michael Teo <miketeo@miketeo.net>
2014-06-01 15:28:29 +0800

    Prepare for 1.1.9 release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ pkgdir = { '': 'python%s' % sys.version_info[0] }
 
 setup(
     name = "pysmb",
-    version = "1.1.8",
+    version = "1.1.9",
     author = "Michael Teo",
     author_email = "miketeo@miketeo.net",
     license = "zlib/libpng",
