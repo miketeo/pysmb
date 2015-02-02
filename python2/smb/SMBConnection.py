7f34c90541d815539107912cfed5f81d2e58295d
ryan <ryan@ryan.(none)>
2015-02-02 11:32:16 -0800

    fixing pep8 spacing

--- python2/smb/SMBConnection.py
+++ python2/smb/SMBConnection.py
@@ -100,7 +100,7 @@ class SMBConnection(SMB):
         self.auth_result = None
         self.sock = socket.socket(sock_family)
         self.sock.settimeout(timeout)
-	self.sock.connect(( ip, port ))
+        self.sock.connect(( ip, port ))
 
         self.is_busy = True
         try:
