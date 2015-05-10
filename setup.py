c09a267a07dbc591bab282861abd52e7a05985f1
Michael Teo <miketeo@miketeo.net>
2015-05-10 14:35:12 +0800

    Bump version and change log for 1.1.16

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ pkgdir = { '': 'python%s' % sys.version_info[0] }
 
 setup(
     name = "pysmb",
-    version = "1.1.15",
+    version = "1.1.16",
     author = "Michael Teo",
     author_email = "miketeo@miketeo.net",
     license = "zlib/libpng",
