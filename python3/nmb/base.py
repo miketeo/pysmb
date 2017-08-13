e900635752fb450ff37d100bd76ed28df4191146
Michael Teo <miketeo@miketeo.net>
2017-08-13 14:07:00 +0800

    Conforms to RFC1002 specs by discarding SESSION_KEEPALIVE NMB packets silently instead of raising warnings on unknown packets

--- python3/nmb/base.py
+++ python3/nmb/base.py
@@ -77,6 +77,9 @@ class NMBSession:
             self.onNMBSessionOK()
         elif packet.type == NEGATIVE_SESSION_RESPONSE:
             self.onNMBSessionFailed()
+        elif packet.type == SESSION_KEEPALIVE:
+            # Discard keepalive packets - [RFC1002]: 5.2.2.1
+            pass
         else:
             self.log.warning('Unrecognized NMB session type: 0x%02x', packet.type)
 
