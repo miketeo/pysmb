e3bbba3501d1b5abe5140c0e93e55e29c26056c1
Michael Teo <miketeo@miketeo.net>
2018-07-28 17:59:41 +0800

    Bump version, changelog and documentation to 1.1.25

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ pkgdir = { '': 'python%s' % sys.version_info[0] }
 
 setup(
     name = "pysmb",
-    version = "1.1.24",
+    version = "1.1.25",
     author = "Michael Teo",
     author_email = "miketeo@miketeo.net",
     license = "zlib/libpng",
