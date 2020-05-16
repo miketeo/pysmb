e62d152e94333e972f911c18bd7098da8a01851f
Michael Teo <miketeo@miketeo.net>
2020-05-16 19:42:45 +0800

    Bump version to 1.1.29 and update release changes in CHANGELOG

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ pkgdir = { '': 'python%s' % sys.version_info[0] }
 
 setup(
     name = "pysmb",
-    version = "1.1.28",
+    version = "1.1.29",
     author = "Michael Teo",
     author_email = "miketeo@miketeo.net",
     license = "zlib/libpng",
