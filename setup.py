aa3683f25ae6d9e51e07779b2ca06f8b0495a54a
Michael Teo <miketeo@miketeo.net>
2020-05-17 20:24:07 +0800

    Bump version to 1.2.1 and update release changes in CHANGELOG

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ pkgdir = { '': 'python%s' % sys.version_info[0] }
 
 setup(
     name = "pysmb",
-    version = "1.2.0",
+    version = "1.2.1",
     author = "Michael Teo",
     author_email = "miketeo@miketeo.net",
     license = "zlib/libpng",
