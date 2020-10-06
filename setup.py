1406ba480c3e8f6c4395a92677e766574ffaab64
Michael Teo <miketeo@miketeo.net>
2020-10-06 19:24:07 +0800

    Bump version to 1.2.4 and update changelog

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ pkgdir = { '': 'python%s' % sys.version_info[0] }
 
 setup(
     name = "pysmb",
-    version = "1.2.3",
+    version = "1.2.4",
     author = "Michael Teo",
     author_email = "miketeo@miketeo.net",
     license = "zlib/libpng",
@@ -17,7 +17,7 @@ setup(
     url = "https://miketeo.net/projects/pysmb",
     package_dir = pkgdir,
     packages = [ 'smb', 'smb.utils', 'nmb' ],
-    install_requires = [ 'pyasn1', 'pycrypto' ],
+    install_requires = [ 'pyasn1' ],
     long_description="""pysmb is an experimental SMB/CIFS library written in Python. It implements the client-side SMB/CIFS protocol which allows your Python application to access and transfer files to/from SMB/CIFS shared folders like your Windows file sharing and Samba folders.""",
     classifiers = [
         "Development Status :: 5 - Production/Stable",
