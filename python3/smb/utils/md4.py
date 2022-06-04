f07ec7d7ef4110a85fbc33e9c98f8240f2f100d3
nig <mail@zentarim.name>
2022-06-04 19:29:41 +0300

    Add tests to an embedded MD4 lib

--- python3/smb/utils/md4.py
+++ python3/smb/utils/md4.py
@@ -23,19 +23,6 @@
 #
 #====================================================================
 
-# MD4 validation data
-
-md4_test= [
-      ('', 0x31d6cfe0d16ae931b73c59d7e0c089c0),
-      ("a",   0xbde52cb31de33e46245e05fbdbd6fb24),
-      ("abc",   0xa448017aaf21d8525fc10ae87aa6729d),
-      ("message digest",   0xd9130a8164549fe818874806e1c7014b),
-      ("abcdefghijklmnopqrstuvwxyz",   0xd79e1c308aa5bbcdeea8ed63df412da9),
-      ("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
-       0x043f8582f241db351ce627e153e7f0e4),
-      ("12345678901234567890123456789012345678901234567890123456789012345678901234567890",
-      0xe33b4ddc9c38f2199c3e7b164fcc0536),
-     ]
 
 #====================================================================
 from .U32 import U32
