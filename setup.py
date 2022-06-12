3b6e9fda170a1b23c7b949db38ae99920dc81495
Michael Teo <miketeo@miketeo.net>
2022-06-12 14:07:58 +0800

    Bump version to 1.2.8 and update CHANGELOG

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ pkgdir = { '': 'python%s' % sys.version_info[0] }
 
 setup(
     name = "pysmb",
-    version = "1.2.7",
+    version = "1.2.8",
     author = "Michael Teo",
     author_email = "miketeo@miketeo.net",
     license = "zlib/libpng",
